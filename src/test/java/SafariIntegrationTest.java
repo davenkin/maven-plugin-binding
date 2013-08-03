dec09fd59eec4c3c3d4710879a9b9788b5c2fbc5
davenkin <davenkin@gmail.com>
2013-08-03 22:17:51 +0800

    added more browser

--- src/test/java/SafariIntegrationTest.java
+++ src/test/java/SafariIntegrationTest.java
@@ -2,7 +2,10 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -20,7 +23,13 @@ public class SafariIntegrationTest {
     @Test
     public void testHelloWorldIndexPage() {
         WebDriver driver = new HtmlUnitDriver();
-        driver.get("http://localhost:8080");
+
+//        WebDriver driver = new SafariDriver();//use safari
+//        WebDriver driver = new InternetExplorerDriver();//use IE
+//        WebDriver driver = new FirefoxDriver();//use fireforx
+
+        driver.get("http://localhost:8080/helloworld.html");
+        System.out.println(driver.getPageSource());
         WebElement element = driver.findElement(By.tagName("h2"));
         assertThat(element.getText(), is("Hello World!"));
     }
